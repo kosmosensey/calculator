37c4f79d6eb7044d65aa6b660b478e2d1aeae90c
kosmo <kosmosensei@gmail.com>
2023-05-05 03:02:20 +0700

    to check(fix)

--- src/main/java/sky/pro/calculator/serves/CalculatorServesImpl.java
+++ src/main/java/sky/pro/calculator/serves/CalculatorServesImpl.java
@@ -25,8 +25,8 @@ public class CalculatorServesImpl implements CalculatorServesInterface {
 
     public String divide(double num1, double num2) {
         if (num2 == 0) {
-                throw new ArithmeticException("Деление на 0");
-            }
+            throw new ArithmeticException("Деление на 0");
+        }
         double result = num1 / num2;
         return num1 + " / " + num2 + " = " + result;
     }
